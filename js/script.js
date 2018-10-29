89fa7daf59ad1b21a9ee40a8a92e6346d90c991c
RomualdasT <tamulaitis.romualdas@gmail.com>
2018-10-29 22:28:07 +0200

    Select changes depending on button feature added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 
 // Add active class
-  var btns = document.getElementsByClassName("sort");
+var btns = document.getElementsByClassName("sort");
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
       var current = document.getElementsByClassName("active");
       current[0].className = current[0].className.replace(" active", "");
       this.className += " active";
+      var value = this.getAttribute("data-value");
+      console.log(value);
+      document.getElementById(value).selected = true;
     });
 };
-
 //Add button value to select and vica verca
