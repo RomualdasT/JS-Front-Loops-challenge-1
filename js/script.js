b7d5037a7a673ace6cfc9623db5a1e2f14d55fde
RomualdasT <tamulaitis.romualdas@gmail.com>
2018-10-30 21:17:45 +0200

    bad code

--- js/script.js
+++ js/script.js
@@ -7,8 +7,17 @@ var btns = document.getElementsByClassName("sort");
       current[0].className = current[0].className.replace(" active", "");
       this.className += " active";
       var value = this.getAttribute("data-value");
-      console.log(value);
       document.getElementById(value).selected = true;
     });
 };
 //Add button value to select and vica verca
+var selects = document.querySelectorAll('select > option');
+for(var z = 0; z < selects.length; z++) {
+  if(selects.selected = true) {
+    var listItem = document.getElementsByClassName('sort');
+    if(this.getAttribute("value") == listItem.getAttribute("data-value")) {
+      listItem.className += " active";
+    };
+  };
+};
+console.log(selects);
