7db62b601a0e5568d2d16b15abff12eac762de55
RomualdasT <tamulaitis.romualdas@gmail.com>
2018-11-03 19:06:34 +0200

    Created two loops + option selectd

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 
 // Add active class
-var btns = document.getElementsByClassName("sort");
-  for (var i = 0; i < btns.length; i++) {
+const btns = document.getElementsByClassName("sort");
+const selected = document.querySelectorAll('option:checked');
+
+
+
+/* for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
       var current = document.getElementsByClassName("active");
+
       current[0].className = current[0].className.replace(" active", "");
       this.className += " active";
-      var value = this.getAttribute("data-value");
-      document.getElementById(value).selected = true;
+
+      let liValue = this.getAttribute("data-value");
+      let selValue = document.getElementById(liValue);
+      selValue.selected = true;
     });
-};
+}; */
 //Add button value to select and vica verca
-var selects = document.querySelectorAll('select > option');
-for(var z = 0; z < selects.length; z++) {
-  if(selects.selected = true) {
-    var listItem = document.getElementsByClassName('sort');
-    if(this.getAttribute("value") == listItem.getAttribute("data-value")) {
-      listItem.className += " active";
-    };
-  };
-};
-console.log(selects);
+
+const selectList = document.getElementById("select-list");
+
+for(var i = 0; i<selectList.options.length;i++){
+  for(var j = 0; j < btns.length; j++){
+    let btnVal = btns[j].getAttribute('data-value');
+    if(selectList[i].value === btnVal) {
+      btns[j].classList.add(" active");
+    }
+  }
+}
